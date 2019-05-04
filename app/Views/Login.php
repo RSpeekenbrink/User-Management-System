bede46facb93db2314312240174b1fcdb91af9bb
Remco Speekenbrink <98remco@gmail.com>
2019-05-04 23:23:14 +0200

    Update Login/Register Form

--- app/Views/Login.php
+++ app/Views/Login.php
@@ -6,23 +6,39 @@
 			<h3>Login</h3>
 			<form action="/login" method="post">
 				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Your Username/Email *" value="" />
+					<input type="text" class="form-control" placeholder="Username/Email" value="" />
 				</div>
 				<div class="form-group">
-					<input type="password" class="form-control" placeholder="Your Password *" value="" />
+					<input type="password" class="form-control" placeholder="Password" value="" />
 				</div>
 				<div class="form-group">
 					<input type="submit" class="btnSubmit" value="Login" />
+
+					<div class="form-check">
+						<input type="checkbox" class="form-check-input" id="remember-me">
+						<label class="form-check-label" for="remember-me">Remember Me</label>
+					</div>
 				</div>
 				<div class="form-group">
-					<a href="#" class="ForgetPwd">Forget Password?</a>
+					<a href="#" class="ForgetPwd">Forgot Password?</a>
 				</div>
 			</form>
 		</div>
 		<div class="col-md-6 login-form-2">
 			<h3>Register</h3>
-			<form>
-				TODO
+			<form action="/register" method="post">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Username" value="" />
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Email" value="" />
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" placeholder="Password" value="" />
+				</div>
+				<div class="form-group">
+					<input type="submit" class="btnSubmit" value="Register" />
+				</div>
 			</form>
 		</div>
 	</div>
